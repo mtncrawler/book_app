019d39fb36a1b77e61c315226b4c4641cf84d060
Jeremy Beck <jeremycbeck@outlook.com>
2018-09-18 09:49:24 -0700

    initial setup of all files

---
+++ server.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const pg = require('pg');
+const express = require('express');
+const ejs = require('ejs');
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+const app = express();
+
+const conString = process.env.DATABASE_URL;
+const client = new pg.Client(conString);
+client.connect();
+client.on('error', error => {
+  console.error(error);
+});
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('./public'));
+
